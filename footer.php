d09c52fe21242d242b2a4dfd502bee208c3e278f
Nagao Sora <ziyuu1014@gmail.com>
2020-12-03 20:17:36 +0900

    ハンバーガーメニューの追加

--- footer.php
+++ footer.php
@@ -1,7 +1,8 @@
-<footer>
+    <footer>
         <p class="copyright">Copyright &copy; DEMO all rights reserved.</p>
     </footer>
     <?php wp_footer(); ?>
+    </main>
 </body>
 
 </html>
--- header.php
+++ header.php
@@ -11,50 +11,22 @@
     <title>サイトのタイトル</title>
 
     <link rel="shortcut icon" href="favicon.ico">
-    <!-- <link rel="stylesheet" type="text/css" media="screen and ( min-width:769px )" href="<?php echo get_template_directory_uri(); ?>./css/style_pc.css"> -->
-	<!-- <link rel="stylesheet" type="text/css" media="screen and ( max-width:768px )" href="<?php echo get_template_directory_uri(); ?>./css/style_sp.css"> -->
-
-    <!-- <script type="text/javascript" src="js/jquery-3.2.1.min.js"></script> -->
-<!-- drawer.css -->
-<!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/drawer/3.2.2/css/drawer.min.css"> -->
-<!-- jquery & iScroll -->
-<!-- drawer.js -->
-<!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/drawer/3.2.2/js/drawer.min.js"></script> -->
-<!-- <script type="text/javascript" src="js/jquery.fadethis.min.js"></script> -->
-
-<!-- <script type="text/javascript" src="js/common.js"></script> -->
 <?php wp_head() ?>
 </head>
 
-<body  class="drawer drawer--right">
-    <header>
-        <nav class="header header-menu">
-            <ul class="flex">
-                <li><a href="/" class="menu-items">TOP</a></li>
-                <li><a href="#sec2-profile" id="sec2" class="menu-items">プロフィール</a></li>
-                <li><a href="#sec3-portfolio" id="sec3" class="menu-items">制作実績</a></li>
-                <li><a href="#sec4-newblog" id="sec4" class="menu-items">新着記事</a></li>
-                <li><a href="#sec5-contactform" id="sec5" class="menu-items">お問い合わせ</a></li>
-                <li><a href="#" id="jp-menu" class="menu-items">日本語</a></li>
-                <li><a href="#" id="en-menu" class="menu-items">English</a></li>
-             </ul>
-        </nav>
-            <button type="button" class="drawer-toggle drawer-hamburger">
-        <span class="sr-only">toggle navigation</span>
-        <span class="drawer-hamburger-icon"></span>
-      </button>
-      <nav class="drawer-nav" role="navigation">
-            <ul class="drawe-menu">
-                <li><a href="#" class="menu-items">TOP</a></li>
-                <li><a href="#sec2-profile" id="sec2" class="menu-items drawer-menu-item">プロフィール</a></li>
-                <li><a href="#sec3-portfolio" id="sec3" class="menu-items drawer-menu-item">制作実績</a></li>
-                <li><a href="#sec4-newblog" id="sec4" class="menu-items drawer-menu-item">新着記事</a></li>
-                <li><a href="#sec5-contactform" id="sec5" class="menu-items drawer-menu-item">お問い合わせ</a></li>
-                <li><a href="#" id="jp-menu" class="menu-items drawer-menu-item">日本語</a></li>
-                <li><a href="#" id="en-menu" class="menu-items drawer-menu-item">English</a></li>
-            
-            </ul>
-        </nav>
-       
-    
-    </header> 
+<body class="drawer drawer--left">
+  <header role="banner">
+    <button type="button" class="drawer-toggle drawer-hamburger">
+      <span class="sr-only">toggle navigation</span>
+      <span class="drawer-hamburger-icon"></span>
+    </button>
+    <nav class="drawer-nav" role="navigation">
+      <ul class="drawer-menu">
+        <li><a class="drawer-brand" href="#">Brand</a></li>
+        <li><a class="drawer-menu-item" href="#">Nav1</a></li>
+        <li><a class="drawer-menu-item" href="#">Nav2</a></li>
+      </ul>
+    </nav>
+  </header>
+  <main role="main">
+    <!-- Page content -->
